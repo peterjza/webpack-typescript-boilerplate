156498bc2b6df9e5350a35347e72439cd9eadc4b
Peter Jarvis <peterjza@gmail.com>
2024-01-25 14:27:26 +0200

    fix: ts config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-	entry: './index.js',
+	entry: './index.ts',
 	mode: 'production',
 	module: {
 		rules: [
