94a36e455a0a891f24de7f2bdfe41eb6b6b43fe3
Peter Jarvis <peterjza@gmail.com>
2024-01-25 14:17:33 +0200

    feat: init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	entry: './index.js',
+	mode: 'production',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.(s(a|c)ss)$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js'],
+	},
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'public'),
+		},
+		compress: true,
+		port: 9000,
+	},
+	plugins: [new HtmlWebpackPlugin()],
+}
